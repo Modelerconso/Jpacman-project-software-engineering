adad7f628b7bf1044aa47b6ee0fe0d4ef34e0f6e
urikpro64 <urikpro64@gmail.com>
2023-03-18 18:34:11 +0700

    feat: add menu

---
+++ src/main/java/nl/tudelft/jpacman/ui/MenuUI.java
@@ -0,0 +1,34 @@
+package nl.tudelft.jpacman.ui;
+
+import nl.tudelft.jpacman.Launcher;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuUI extends JFrame {
+    private JPanel menuUI;
+
+    public MenuUI() {
+        menuUI = new JPanel();
+        JLabel menuLabel = new JLabel("Menu", JLabel.CENTER);
+        JButton onlineButton = new JButton("Online");
+        JButton offlineButton = new JButton("Offline");
+        offlineButton.addActionListener(e -> {
+            Launcher launchers = new Launcher();
+            launchers.launch();
+            dispose();
+        });
+        JButton exitButton = new JButton("exit");
+        menuUI.setLayout(new GridLayout(4,1));
+        menuUI.add(menuLabel);
+        menuUI.add(onlineButton);
+        menuUI.add(offlineButton);
+        menuUI.add(exitButton);
+        setSize(200,500);
+        add(menuUI);
+    }
+
+    public void start() {
+       setVisible(true);
+    }
+}
